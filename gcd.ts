c4ab5528f007ecd4d92f8b66571e5ca30a3867b6
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2025-02-09 22:21:32 +0530

    feat: implement the extended euclid algo

--- gcd.ts
+++ gcd.ts
@@ -14,10 +14,23 @@ export function lcm(a: number, b: number) {
     return a / gcd(a, b) * b;
 }
 
+export function extendedEuclid(a: number, b: number, x: number = 0, y: number = 0): [number, number, number] {
+    if (b == 0) {
+        x = 1
+        y = 0
+        return [a, x, y];
+    }
+    const [ans, x1, y1] = extendedEuclid(b, a % b, x, y)
+    x = y1;
+    y = x1 - y1 * Math.floor(a / b);
+    return [ans, x, y]
+}
+
 
 if (import.meta.main) {
     console.log(gcd(3, 9))
     console.log(gcd(9, 3))
     console.log(gcdNormalize(10, 15))
     console.log(lcm(10, 15))
+    console.log(extendedEuclid(55, 80))
 }
