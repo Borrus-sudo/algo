12d5f3811f16393d2be6e957defcd7a5ecbffa75
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2025-02-26 12:38:35 +0530

    feat: push graycode solution

--- cses/graycode.cpp
+++ cses/graycode.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-int main () {
-    // efficiently generating performing binary additions from bitsets. 
-    // 00 
-    // 01 
+
+vector<string> gen(vector<string> span, int currLevel, int toReach)
+{
+    if (currLevel == toReach)
+        return span;
+    vector<string> result;
+    for (int i = 0; i < span.size(); i++)
+    {
+        char firstElem = i & 1 ? '1' : '0';
+        char secondElem = i & 1 ? '0' : '1';
+        result.push_back(span[i] + firstElem);
+        result.push_back(span[i] + secondElem);
+    }
+    return gen(result, ++currLevel, toReach);
+}
+
+int main()
+{
+    int depth;
+    cin >> depth;
+    vector<string> results = gen({"0", "1"}, 1, depth);
+    for (auto str : results)
+        cout << str << endl;
     return 0;
 }
