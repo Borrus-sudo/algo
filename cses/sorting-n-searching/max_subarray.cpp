4163f23545be87202ccd91465f5deb68ab4496c6
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2025-06-04 01:45:12 +0530

    feat: solve a couple qns

---
+++ cses/sorting-n-searching/max_subarray.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <climits>
+
+#define all(x) (x).begin, (x).end()
+#define dbg(x) cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+
+using namespace std;
+
+template <typename S, typename T>
+void smax(S &a, const T &b)
+{
+    if (a < b)
+        a = b;
+};
+
+template <typename S, typename T>
+void smin(S &a, const T &b)
+{
+    if (a > b)
+        a = b;
+};
+
+using ll = long long;
+const int m = 1e9 + 7;
+
+void solve()
+{
+    // write your code here
+    int n;
+    cin >> n;
+    ll running_sum = 0, ans = LONG_MIN;
+    while (n--)
+    {
+        int curr;
+        cin >> curr;
+        running_sum += curr;
+        smax(ans, running_sum);
+        if (running_sum < 0)
+            running_sum = 0;
+    }
+    cout << ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int TC = 1;
+    // cin >> TC;
+
+    while (TC--)
+        solve();
+
+    return 0;
+}
---
+++ cses/sorting-n-searching/sticks.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+
+#define all(x) (x).begin, (x).end()
+#define dbg(x) cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+
+using namespace std;
+
+template <typename S, typename T>
+void smax(S &a, const T &b)
+{
+    if (a < b)
+        a = b;
+};
+
+template <typename S, typename T>
+void smin(S &a, const T &b)
+{
+    if (a > b)
+        a = b;
+};
+
+using ll = long long;
+const int m = 1e9 + 7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> lens(n, 0);
+    for (int &len : lens)
+        cin >> len;
+    sort(lens.begin(), lens.end());
+    int median = lens[n / 2];
+    ll cost = 0;
+    for (int len : lens)
+        cost += abs(len - median);
+    cout << cost;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int TC = 1;
+    // cin >> TC;
+
+    while (TC--)
+        solve();
+
+    return 0;
+}
