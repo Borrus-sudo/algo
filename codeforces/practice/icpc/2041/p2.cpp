080a0b70d178ac98b82f408a95a5ae89d42c03c8
Jinmay <jinmayjhaveri006@gmail.com>
2025-07-27 16:01:55 +0530

    feat: try to solve qns

---
+++ codeforces/practice/icpc/2041/p2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <queue>
+#include <unordered_set>
+
+#define all(x) (x).begin(), (x).end()
+#define dbg(x) cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+
+using namespace std;
+
+template <typename S, typename T>
+void smax(S &a, const T &b)
+{
+    if (a < b)
+        a = b;
+};
+
+template <typename S, typename T>
+void smin(S &a, const T &b)
+{
+    if (a > b)
+        a = b;
+};
+
+using ll = long long;
+const int INF = 1e9 + 7;
+
+void solve()
+{
+    double a, b;
+    cin >> a >> b;
+    double n = (-1 + sqrt(8 * (a + b) + 1)) / 2.0;
+    ll res = (ll)n;
+    cout << res << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int TC = 1;
+    cin >> TC;
+
+    while (TC--)
+        solve();
+
+    return 0;
+}
---
+++ codeforces/practice/icpc/2041/p8.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <queue>
+#include <unordered_set>
+
+#define all(x) (x).begin(), (x).end()
+#define dbg(x) cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+
+using namespace std;
+
+template <typename S, typename T>
+void smax(S &a, const T &b)
+{
+    if (a < b)
+        a = b;
+};
+
+template <typename S, typename T>
+void smin(S &a, const T &b)
+{
+    if (a > b)
+        a = b;
+};
+
+using ll = long long;
+const int INF = 1e9 + 7;
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+    for (int &elem : arr)
+        cin >> elem;
+    int minIdx = min_element(all(arr)) - arr.begin();
+    // sort from 0 to minIdx
+    int maxIdx = max_element(all(arr)) - arr.begin();
+    // whoever is closer to the centre
+     
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int TC = 1;
+    // cin >> TC;
+
+    while (TC--)
+        solve();
+
+    return 0;
+}
