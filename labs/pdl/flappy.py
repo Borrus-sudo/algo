c9806e3f39914866baa70fe8bfa2e5e3202df391
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2025-11-02 21:16:13 +0530

    feat: make the game multiplatform

--- labs/pdl/flappy.py
+++ labs/pdl/flappy.py
@@ -1,8 +1,68 @@
 import random
 import sys
-import msvcrt
+import os
 
-# components
+# Windows
+if os.name == 'nt':
+    import msvcrt
+# Posix (Linux, OS X)
+else:
+    import sys
+    import termios
+    import atexit
+    from select import select
+
+
+class KBHit:
+
+    def __init__(self):
+        if os.name == 'nt':
+            pass
+        else:
+            # Save the terminal settings
+            self.fd = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.fd)
+            self.old_term = termios.tcgetattr(self.fd)
+            # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+            # Support normal-terminal reset at exit
+            atexit.register(self.set_normal_term)
+
+
+    def set_normal_term(self):
+        ''' Resets to normal terminal.  On Windows this is a no-op.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+
+    def getch(self):
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow().
+        '''
+        s = ''
+
+        if os.name == 'nt':
+            return msvcrt.getch().decode('utf-8')
+
+        else:
+            return sys.stdin.read(1)
+
+    def kbhit(self):
+        ''' Returns True if keyboard character was hit, False otherwise.
+        '''
+        if os.name == 'nt':
+            return msvcrt.kbhit()
+
+        else:
+            dr,dw,de = select([sys.stdin], [], [], 0)
+            return dr != []
+
 
 
 class Velocity:
@@ -185,17 +245,19 @@ def update(loc: list[Dimensions], vels: list[Velocity]):
         item.x += vel.dx
         item.y += vel.dy
 
+kbhit = KBHit()
 
 # key logging system
 def key_press(bird: Dimensions):
-    if msvcrt.kbhit():
-        msvcrt.getch()
+    if kbhit.kbhit():
+        kbhit.getch()
         bird.y -= 5  # to account for the gravity
+    pass
 
 
 def was_pressed():
-    if msvcrt.kbhit():
-        msvcrt.getch()
+    if kbhit.kbhit():
+        kbhit.getch()
         return True
     return False
 
