5ffb64cf46ce9beee3fa9961e4c0e9dddcbeee94
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2026-04-05 21:52:33 +0530

    feat: finish code 🥳

--- labs/ai/tic_tac_toe.py
+++ labs/ai/tic_tac_toe.py
@@ -5,12 +5,77 @@ import tkinter as tk
 from tkinter import ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from lib import Search
+
+
+class Search:
+
+    @staticmethod
+    def layered_layout(G):
+        levels = {}
+        for node, data in G.nodes(data=True):
+            lvl = data.get("depth", 0)
+            levels.setdefault(lvl, []).append(node)
+
+        pos = {}
+        for lvl, nodes in levels.items():
+            for i, node in enumerate(nodes):
+                pos[node] = (i / 2, -lvl)
+
+        return pos
+
+    def __init__(self, G):
+        self.G = G
+
+    # hashify for when the node is not hashable (cause it is mutable). So we provide a custom hashify function
+    def hashify(self, node):
+        return node
+
+    def info(self, node, metadata):
+        if self.hashify(node) not in self.G.nodes:
+            return
+        for k, v in metadata.items():
+            self.G.nodes[self.hashify(node)][k] = v
+
+    def track(self, node):
+        if self.hashify(node) not in self.G.nodes:
+            self.G.add_node(self.hashify(node))
+
+    def link(self, parent, child):
+        parent_h = self.hashify(parent)
+        child_h = self.hashify(child)
+        if parent_h in self.G.nodes and child_h in self.G.nodes:
+            self.G.add_edge(parent_h, child_h)
+
+    def backtrack(self, node):
+        self.G.remove_node(self.hashify(node))
+
+    def getGraph(self):
+        return self.G
+
+    def render(self):
+        args = {
+            "G": self.G,
+            "node_color": "lightblue",
+            "node_size": 1500,
+            "font_size": 8,
+            "labels": nx.get_node_attributes(self.G, "labels"),
+            "arrows": True,
+        }
+        return args
 
 
 class Minmax(Search):
     def __init__(self, G=nx.DiGraph()):
         super().__init__(G)
+
+    def pretty_print(self, board):
+        res = ""
+        for i in range(3):
+            row = " | ".join(board[i])
+            res += "\n " + row
+            if i < 2:
+                res += "\n---+---+---"
+        return res
 
     # shit show on the memory, a major one!
     def generator(self, state):
@@ -20,17 +85,17 @@ class Minmax(Search):
         for row in range(dim):
             for col in range(dim):
                 if board[row][col] is TicTacToe.VOID:
-                    # I can play a move?
                     new_neighbour = copy.deepcopy(board)
                     new_neighbour[row][col] = curr
-                    # if we have explored this before, we need to make sure that we use the cached, value!
                     res.append(new_neighbour)
         return res
 
-    # this is a total shit show on the memory
-    def search(self, state, alpha=-math.inf, beta=math.inf):
-        board, curr = state
-        # this stuff is not really good for perf, but it's fine ig
+    def search(self, state, alpha=-math.inf, beta=math.inf, depth=0):
+        board, turn = state
+        if depth < 5:
+            self.track(board)
+            self.info(board, {"label": self.pretty_print(board), "depth": depth})
+
         if TicTacToe.eval(board) == TicTacToe.P1:
             return (1, board)
         elif TicTacToe.eval(board) == TicTacToe.P2:
@@ -38,24 +103,34 @@ class Minmax(Search):
         elif TicTacToe.eval(board) == TicTacToe.TIE:
             return (0, board)
         else:
-            if curr is TicTacToe.P1:
+            if turn is TicTacToe.P1:
                 maxima = -math.inf
                 goto = None
+
                 for neighbour in self.generator(state):
-                    new_val, _ = self.search((neighbour, TicTacToe.P2), alpha, beta)
+                    new_val, _ = self.search(
+                        (neighbour, TicTacToe.P2), alpha, beta, depth + 1
+                    )
                     alpha = max(alpha, new_val)
+                    self.link(board, neighbour)
                     if maxima < new_val:
                         maxima = new_val
                         goto = neighbour
+
                     if beta <= alpha:
                         break
+
                 return (maxima, goto)
             else:
                 minima = math.inf
                 goto = None
+
                 for neighbour in self.generator(state):
-                    new_val, _ = self.search((neighbour, TicTacToe.P1), alpha, beta)
+                    new_val, _ = self.search(
+                        (neighbour, TicTacToe.P1), alpha, beta, depth + 1
+                    )
                     beta = min(beta, new_val)
+                    self.link(board, neighbour)
                     if minima > new_val:
                         minima = new_val
                         goto = neighbour
@@ -68,9 +143,6 @@ class Minmax(Search):
 
 
 class TicTacToe:
-    """
-    We give ourselves all the primitives that we need for the game loop!
-    """
 
     # Constants
     VOID = "*"
@@ -78,12 +150,6 @@ class TicTacToe:
     ON_GOING = "ON_GOING"
     P1 = "X"
     P2 = "O"
-
-    """
-    Possible Results: 
-    player 1 won, player 2 won, tie or still on-going!
-    Does not check the validity of the position. We just expect that you that it is a valid position!
-    """
 
     @staticmethod
     def eval(board):
@@ -185,7 +251,7 @@ class TicTacToe:
     def generateNextMove(self, G):
         _, best_next = TicTacToe.search(
             self.board, self.CURR, G
-        )  # should also give modify G, prolly not
+        )  # should not modify G, but it does!
         if best_next is None:
             return (-1, -1)
         for row in range(self.dim):
@@ -218,9 +284,9 @@ class Game:
         left = ttk.Frame(self.root)
         left.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
 
-        left.rowconfigure(0, weight=0)  # title
-        left.rowconfigure(1, weight=3)  # board (big)
-        left.rowconfigure(2, weight=1)  # log
+        left.rowconfigure(0, weight=0)
+        left.rowconfigure(1, weight=3)
+        left.rowconfigure(2, weight=1)
         left.columnconfigure(0, weight=1)
 
         ttk.Label(
@@ -239,7 +305,9 @@ class Game:
                 btn = tk.Button(
                     board_frame,
                     text="",
-                    command=lambda row=row, col=col: self.game_update((row, col), TicTacToe.P1),
+                    command=lambda row=row, col=col: self.game_update(
+                        (row, col), TicTacToe.P1
+                    ),
                 )
                 btn.grid(row=row, column=col, sticky="nsew", padx=2, pady=2)
                 self.buttons[row][col] = btn
@@ -256,7 +324,7 @@ class Game:
         self.log_box.see(tk.END)
 
     def game_update(self, pos, playing):
-     
+
         row, col = pos
         playerSymbol = self.game.CURR
         done = self.game.updateMove(pos, playing)
@@ -264,9 +332,9 @@ class Game:
             self.log("Invalid move vro!")
         else:
             self.buttons[row][col].configure(text=playerSymbol)
-        
+
         curr_state = self.game.getGameState()
-        
+
         if curr_state == TicTacToe.TIE:
             self.log("Game draw!")
         elif curr_state == TicTacToe.P1:
@@ -274,11 +342,28 @@ class Game:
         elif curr_state == TicTacToe.P2:
             self.log("You lost!")
         elif self.game.CURR == TicTacToe.P2:
+            self.G.clear()
             self.game_update(self.game.generateNextMove(self.G), TicTacToe.P2)
+            self.renderGraph()
 
     def ui(self):
         self.root.mainloop()
 
+    def renderGraph(self):
+        self.ax.clear()
+        nx.draw(
+            G=self.G,
+            pos=Search.layered_layout(self.G),
+            ax=self.ax,
+            node_size=500,
+            labels=nx.get_node_attributes(self.G, "label"),
+            font_size=8,
+            node_color="lightblue",
+            arrows=True,
+        )
+        self.ax.set_title("Minmax TicTacToe")
+        self.ax.set_axis_off()
+        self.canvas.draw()
 
 if __name__ == "__main__":
     gg = Game()
